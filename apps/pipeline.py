cbf46f71153d28f64e98b3a4bb4fe981ff66b91e
Rich Smith <rls@hwyl.org>
2014-04-24 17:34:28 +0100

    Add pipeline app

--- apps/pipeline.py
+++ apps/pipeline.py
@@ -2,12 +2,26 @@ import os
 
 from model.app import app
 from model import iotypes
-from control import access
+from control import access, parser
 
 @app(name = "pipeline",
      input = None,
-     output = iotypes.Html)
+     output = iotypes.Text,
+     preferred_renderer = 'ol')
 def execute(input, options, environment):
 
     command_str = " ".join(options)
-    return "This command disabled until the new syntax is worked out"
+    pipeline = parser.create_pipeline(command_str)
+
+    output = []
+    for command in pipeline.commands:
+        output.append(describe(command))
+
+    return output
+    #return "This command disabled until the new syntax is worked out"
+
+def describe(command):
+    description = command.app.name
+    description += ' '
+    description += str(command.options)
+    return description
