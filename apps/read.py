f50ca697ff4d6a55ab79471bb1d4bf44af7aaac8
Rich Smith <rls@hwyl.org>
2014-05-01 23:21:55 +0100

    Simplified read app

--- apps/read.py
+++ apps/read.py
@@ -8,12 +8,5 @@ from control import access
      input = iotypes.File,
      output = iotypes.Text)
 def execute(input, options, environment):
-    assert len(options) == 0
 
-    path = access.interpret_path(options[0])
-    if access.is_valid_path(path):
-        url = access.path_to_url(path)
-        print "url is " + url
-        return '<div class="show"><img src="' + url + '"/></div>'
-    else:
-        raise RuntimeError("Not valid " + options[0])
+    return access.read_file(input.path)
