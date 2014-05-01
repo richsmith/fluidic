0867722648325f14c5db131e392d745183c2abb4
Rich Smith <rls@hwyl.org>
2014-05-01 23:40:46 +0100

    start the session with a ls

--- fluidic.py
+++ fluidic.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 
-# (C) Rich Smith 2012
+# (C) Rich Smith 2014
 # rls@hwyl.org
 
 import os
 from view import ui
-from control import access, output, parser
+from control import access, output, processor
 
 name = "Fluidic"
 version = "0.4.0"
-
-def print_home():
-    from apps import ls
-    ls.execute(None, None, None)
 
 if __name__ == '__main__':
     path = access.interpret_path("~")
@@ -21,8 +17,12 @@ if __name__ == '__main__':
     output.clear()
     output.welcome()
 
-    print_home()
+    ls_output = processor.process('ls');
+    output.print_outputs(ls_output)
+    
+
     ui.setup()
+
 
 
 
