3a07981ef35b34a2004ee76d1ccf86c49307c72c
Rich Smith <rls@hwyl.org>
2014-05-09 07:46:44 +0100

    Better copying and moving

--- apps/copy.py
+++ apps/copy.py
@@ -1,19 +1,35 @@
 import os
 import shutil
 
+from control import access
 from model.app import app
 from model import iotypes
 
 @app(name = 'copy',
      aliases = ['cp', '+>'],
      input = iotypes.File,
-     output = None)
+     output = iotypes.Nothing)
 def execute(input, options, environment):
 
-    source = input.path
+    source = get_source(input)
     destination = get_destination(options)
 
-    shutil.copy2(source, destination)
+    fn = None
+
+    if isinstance(input, iotypes.Directory):
+        fn = shutil.copytree
+    elif isinstance(input, iotypes.File):
+        fn = shutil.copy2
+    else:
+        raise Exception("Attempted to copy an invalid object (not a file?)")
+
+    fn(source, destination)
+
+def get_source(input):
+    source = input.path
+    if not access.is_valid_path(source):
+        raise Exception("Attempted to copy from an invalid path")
+    return source
 
 
 def get_destination(options):
--- apps/remove.py
+++ apps/remove.py
@@ -1,16 +1,33 @@
 import os
 import shutil
 
+from control import access
 from model.app import app
 from model import iotypes
 
 @app(name = 'remove',
      aliases = ['rm'],
      input = iotypes.File,
-     output = None)
+     output = iotypes.Nothing)
 def execute(input, options, environment):
 
-    print "Would have deleted " + str(input.path)
+    source = get_source(input)
 
-    # os.remove(input.path)
-    # os.removedirs(input.path)
+    fn = None
+
+    if isinstance(input, iotypes.Directory):
+        fn = shutil.rmtree
+    elif isinstance(input, iotypes.File):
+        fn = os.remove
+    else:
+        raise Exception("Attempted to delete an invalid object (not a file?)")
+
+    fn(input.path)
+
+
+
+def get_source(input):
+    source = input.path
+    if not access.is_valid_path(source):
+        raise Exception("Attempted to delete an invalid path")
+    return source
