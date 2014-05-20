de4463e2c43f64bac9bf387ed6bd302dc523aed7
Rich Smith <rls@hwyl.org>
2014-05-20 08:13:40 +0100

    clear output on exit

--- apps/exit.py
+++ apps/exit.py
@@ -1,11 +1,14 @@
 import sys
+import control
 from model import iotypes
 from model.app import app
+
 
 @app(name = "exit",
      input = iotypes.Nothing,
      output = iotypes.Nothing)
 def execute(files, options, environment):
+    control.output.clear()
     sys.exit(0)
 
 
