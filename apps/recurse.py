149493b5c88d77862a510845e3cab37556bc1faf
Rich Smith <rls@hwyl.org>
2014-06-04 08:00:24 +0100

    Fix recursion to be nicer and to include dirs

--- apps/recurse.py
+++ apps/recurse.py
@@ -13,23 +13,36 @@ def execute(input, options, environment):
     files = []
 
     for file in input:
-        files.append(file)
-        root_path = file.path
-        if isinstance(file, iotypes.Directory):
-            to_add = get_files_in(file.path)
-            for sub_file in to_add:
-                files.append(sub_file)
+        path = file.path
+        to_add = get_files_in(path)
+        for new_file in to_add:
+            files.append(new_file)
 
     return files
 
 
 def get_files_in(root_path):
+
     files = []
-    for root, sub_dirs, filenames in os.walk(root_path):
+
+    def add_file(path):
+        file_object = get_file(path)
+        files.append(file_object)
+
+    add_file(root_path)
+
+    for root, dirs, filenames in os.walk(root_path):
+
+        for dir in dirs:
+            path = os.path.join(root, dir)
+            add_file(path)
+
         for filename in filenames:
             path = os.path.join(root, filename)
-            #yield iotypes.File()
-            file_object = file.execute(None, [path], None)[0]
-            files.append(file_object)
+            add_file(path)
 
     return files
+
+
+def get_file(path):
+    return file.execute(None, [path], None)[0]
